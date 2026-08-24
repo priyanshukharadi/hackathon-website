bf39b757bbe577f86af247eaa0c41871ab5cb040
priyanshu <priyanshukharadi@email.com>
2026-08-24 15:16:21 +0530

    Create ShopEasy homepage

--- static/script.js
+++ static/script.js
@@ -1,4 +1,21 @@
-function showMessage() {
-    document.getElementById("message").textContent =
-        "JavaScript is working! 🔥";
+let cartCount = 0;
+
+
+function addToCart() {
+
+    cartCount++;
+
+    document.getElementById("cart-count").textContent = cartCount;
+
+    alert("Product added to cart! 🛒");
+
+}
+
+
+function scrollToProducts() {
+
+    document.getElementById("products").scrollIntoView({
+        behavior: "smooth"
+    });
+
 }
